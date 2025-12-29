4d01339f88fb0b483bb954003e110de431ffbc1e
rakhi <rakhisingh876.com@gmail.com>
2025-12-29 19:38:47 +0530

    implement user login with JWT authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,4 +33,25 @@ async function registerUser(req, res) {
     });
 }
 
-module.exports = { registerUser };
+async function loginUser(req,res){
+    const { email, password }= req.body;
+
+    const user = await userModel.findOne({
+        email
+    })
+    if(!user){
+        return res.status(400).json({message:"Invalid email or password"});
+    }
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
+        res.cookie("token",token);
+        res.status(200).json({
+        message: "Login successful",
+        user: {
+            _id: user._id,
+            email: user.email,
+            fullname: user.fullname}
+        
+    });
+    }
+
+module.exports = { registerUser, loginUser };
