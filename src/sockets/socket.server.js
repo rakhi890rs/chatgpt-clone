9a40f5a3b050cac6af5b720bfafda0a6984851e7
rakhi <rakhisingh876.com@gmail.com>
2026-01-20 11:03:21 +0530

    feat(socket): add JWT authentication middleware for Socket.IO

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -1,21 +1,34 @@
 const { Server } = require("socket.io");
-
+const cookie = require("cookie");
+const jwt=require('jsonwebtoken');
+const userModel= require('../models/user.model')
 function initSocketServer(httpServer) {
-    const io = new Server(httpServer, {
-        cors: {
-            origin: "*", // change later for security
+    const io = new Server(httpServer, {});
+    io.use(async (socket, next) => {
+        const cookieHeader = socket.handshake.headers?.cookie;
+        if (!cookieHeader) {
+            return next(new Error("Authentication error: No cookies"));
+        }
+        const cookies = cookie.parse(cookieHeader);
+        if (!cookies.token) {
+            return next(new Error("Authentication error: No token provided"));
+        }
+        try{
+            const decoded= jwt.verify(cookies.token,process.env.JWT_SECRET);
+            const user = await userModel.findById(decoded.id);
+            socket.user= user;
+            next();
+
+        }catch(err){
+             return next(new Error("Authentication error: Invalid token"));
         }
     });
-
     io.on("connection", (socket) => {
         console.log("New socket connection:", socket.id);
-
-        socket.on("disconnect", () => {
-            console.log("User disconnected:", socket.id);
-        });
     });
-
-    return io;
 }
 
 module.exports = initSocketServer;
+
+
+// io.use() is a Socket.IO middleware that executes during the handshake phase, before a socket connection is established, and is commonly used for authentication and authorization.
