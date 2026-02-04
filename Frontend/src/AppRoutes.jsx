7ba7b13b33a8f407c0284e609ab11bd3761759dc
rakhi <rakhisingh876.com@gmail.com>
2026-02-04 23:12:07 +0530

    fix(frontend): enable proper routing for Home, Register, and Login pages

---
+++ Frontend/src/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/register' element={<h1>Register</h1>} />
+        <Route path='/login' element={<h1>Login</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
