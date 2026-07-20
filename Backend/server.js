3f019137d177bbbb47a024c94ae5717fcfe816f7
rakhi <rakhisingh876.com@gmail.com>
2026-07-20 17:47:25 +0530

    Prepare backend for Render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-const app = require('./src/app');
-const connectDB = require('./src/db/db');
+require("dotenv").config();
+
+const app = require("./src/app");
+const connectDB = require("./src/db/db");
 const initSocketServer = require("./src/sockets/socket.server");
 const http = require("http");
 
@@ -10,16 +11,18 @@ connectDB();
 // Create HTTP server
 const httpServer = http.createServer(app);
 
-// Initialize socket
+// Initialize Socket.IO
 initSocketServer(httpServer);
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
-// 
-//
+// Use Render's PORT or local 3000
+const PORT = process.env.PORT || 3000;
 
 // Start server
-httpServer.listen(3000, () => {
-    console.log("Server running on port 3000");
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
